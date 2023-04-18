f9a8008868f179b446c35814a926ac0c000c01ea
Shahbaz Ansari <96674638+shahbaz4783@users.noreply.github.com>
2023-04-18 15:22:34 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Random Number Generator for Player 1
+let randomNum1 = Math.floor(Math.random() * 6) + 1;
+let randomImg1 = "/images/dice" + randomNum1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImg1);
+
+// Random Number Generator for Player 2
+let randomNum2 = Math.floor(Math.random() * 6) + 1;
+let randomImg2 = "/images/dice" + randomNum2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
+
+
+// Giving Action to Play Button
+document.querySelector(".btn").addEventListener("click", function () {
+    location.reload();
+})
+
+
+// Manupulating DOM
+if (randomNum1 > randomNum2) {
+    document.querySelector(".result").innerHTML = "Winner: Player 🅰️";
+    document.querySelector(".img1").style.cssText = "border: 5px solid green; border-radius: 50px";
+
+} else if (randomNum1 < randomNum2) {
+    document.querySelector(".result").innerHTML = "Winner: Player 🅱️";
+    document.querySelector(".img2").style.cssText = "border: 5px solid green; border-radius: 50px";
+
+} else {
+    document.querySelector(".result").innerHTML = "It's a Tie! 🤝";
+    document.querySelectorAll(".img1, .img2").forEach(function(img){
+    img.style.cssText = "border: 5px solid green; border-radius: 50px";
+    })
+}
