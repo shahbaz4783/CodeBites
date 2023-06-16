2ee08255614f05223af20e790246922bbc9c2e9f
shahbaz4783 <shahbaz4783@gmail.com>
2023-06-17 01:25:20 +0530

    use nav on all pages without copy paste

--- authentix/navbar.js
+++ authentix/navbar.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+    // Load the nav content
     fetch('navbar.html')
       .then(response => response.text())
       .then(data => {
-        document.getElementById('navContainer').innerHTML = data;
+        document.getElementById('navigation').innerHTML = data;
       });
-
-  });
   
+    // Load the footer content
+    fetch('footer.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('footer').innerHTML = data;
+      });
+  });
+
