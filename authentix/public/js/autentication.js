2b9174404ada1386e7e01a3c094dea79cde17768
shahbaz4783 <shahbaz4783@gmail.com>
2023-06-17 21:09:17 +0530

    some files added

--- authentix/public/js/autentication.js
+++ authentix/public/js/autentication.js
@@ -2,8 +2,8 @@ const loginForm = document.getElementById('loginForm');
 const signupForm = document.getElementById('signupForm');
 
 document.addEventListener('DOMContentLoaded', function () {
-    const loginToggle = document.getElementById('loginToggle');
-    const signupToggle = document.getElementById('signupToggle');
+	const loginToggle = document.getElementById('loginToggle');
+	const signupToggle = document.getElementById('signupToggle');
 
 	loginForm.style.display = 'none';
 	signupForm.style.display = 'block';
@@ -19,46 +19,83 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 });
 
+// eg
+
+let form = document.querySelector('#myForm');
+let name = document.querySelector('#name');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+
+let LSData = JSON.parse(localStorage.getItem('sign-up')) || [];
+
+form.addEventListener('submit', function (a) {
+	a.preventDefault();
+
+	let obj = {
+		name: name.value,
+		id: Math.random() * 1000 + 1 + name.value,
+		email: email.value,
+		password: password.value,
+	};
+
+	for (let i = 0; i < LSData.length; i++) {
+		if (email.value != '' && LSData[i].email === email.value) {
+			return alert('Email already exist!');
+		}
+	}
+	if (email.value == '' && password.value == '')
+		alert('Please enter valid email');
+	if (email.value != '' && password.value != '') {
+		alert('Account created successfully');
+		LSData.push(obj);
+		localStorage.setItem('sign-up', JSON.stringify(LSData));
+	}
+});
+
+//my
 
 signupForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const username = document.getElementById('signupUsername').value;
-    const email = document.getElementById('signupEmail').value;
-    const password = document.getElementById('signupPassword').value;
+	event.preventDefault();
+	const username = document.getElementById('signupUsername').value;
+	const email = document.getElementById('signupEmail').value;
+	const password = document.getElementById('signupPassword').value;
 
-    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+	let LSData = JSON.parse(localStorage.getItem('formData')) || [];
 
-    let exist = formData.length && 
-        JSON.parse(localStorage.getItem('formData')).some(data => 
-            data.username.toLowerCase() == username.toLowerCase()
-        );
 
-        if(!exist){
-            formData.push({ username, email, password });
-            localStorage.setItem('formData', JSON.stringify(formData));
-            document.getElementById('signupForm').reset();
-            document.getElementById('username').focus();
-            alert("Account Created.\n\ Login NOW");
-            loginForm.style.display = 'block';
-            signupForm.style.display = 'none';
-        }
-        else{
-            alert("This email already exist");
-        }
-  });
+		let obj = {
+			name: name.value,
+			id: Math.random() * 1000 + 1 + name.value,
+			email: email.value,
+			password: password.value,
+		};
 
+		for (let i = 0; i < LSData.length; i++) {
+			if (email.value != '' && LSData[i].email === email.value) {
+				return alert('Email already exist!');
+			}
+		}
+		if (email.value == '' && password.value == '')
+			alert('Please enter valid email');
+		if (email.value != '' && password.value != '') {
+			alert('Account created successfully');
+			LSData.push(obj);
+			localStorage.setItem('sign-up', JSON.stringify(LSData));
+		}
+	
+});
 
 loginForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const email = document.getElementById('loginEmail').value;
-    const password = document.getElementById('loginPassword').value;
-    const storedEmail = localStorage.getItem('email');
-    const storedPassword = localStorage.getItem('password');
-  
-    if (email === storedEmail && password === storedPassword) {
-      alert('Login successful!');
-      window.location.href = '/authentix/index.html';
-    } else {
-      alert('Incorrect email or password. Please try again.');
-    }
-  });
+	event.preventDefault();
+	const email = document.getElementById('loginEmail').value;
+	const password = document.getElementById('loginPassword').value;
+	const storedEmail = localStorage.getItem('email');
+	const storedPassword = localStorage.getItem('password');
+
+	if (email === storedEmail && password === storedPassword) {
+		alert('Login successful!');
+		window.location.href = '/authentix/index.html';
+	} else {
+		alert('Incorrect email or password. Please try again.');
+	}
+});
--- authentix/views/template.js
+++ authentix/views/template.js
@@ -11,3 +11,24 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('footer').innerHTML = data;
       });
   });
+
+
+
+  const token = localStorage.getItem('token');
+  const getJoin = document.querySelector('#join');
+  const getProfile = document.querySelector('#profile');
+  const getNav = document.querySelector('#navigation');
+
+  if (true) {
+    // getJoin.style.display = 'none';
+    // getProfile.style.display = 'flex';
+    console.log(token.length);
+    
+    const login = document.createElement('p').innerText = 'LogIn'
+    getNav.append(login)
+  }
+
+
+  // document.querySelector('#logout').addEventListener('click', () => {
+  //   localStorage.removeItem('token');
+  // })
