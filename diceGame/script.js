c39ccfac2639c12d8799446e273ee779e2ab891b
shahbaz4783 <shahbaz4783@gmail.com>
2023-06-19 01:24:28 +0530

    dice game fixes

--- diceGame/script.js
+++ diceGame/script.js
@@ -1,34 +1,37 @@
-// Random Number Generator for Player 1
-let randomNum1 = Math.floor(Math.random() * 6) + 1;
+let randomNum1;
 let randomImg1 = "/diceGame/images/dice" + randomNum1 + ".png";
-document.querySelectorAll("img")[0].setAttribute("src", randomImg1);
 
-// Random Number Generator for Player 2
-let randomNum2 = Math.floor(Math.random() * 6) + 1;
+let randomNum2;
 let randomImg2 = "/diceGame/images/dice" + randomNum2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
 
 
-// Giving Action to Play Button
-document.querySelector(".btn").addEventListener("click", function () {
-      setTimeout(function(){
-        location.reload();
-    }, 200);
-})
+const rollDice = () => {
+  // Random Number Generator for Player 1
+  randomNum1 = Math.floor(Math.random() * 6) + 1;
+  randomImg1 = "/diceGame/images/dice" + randomNum1 + ".png";
+  document.querySelectorAll("img")[0].setAttribute("src", randomImg1);
+
+  // Random Number Generator for Player 2
+  randomNum2 = Math.floor(Math.random() * 6) + 1;
+  randomImg2 = "/diceGame/images/dice" + randomNum2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
 
 
-// Manupulating DOM
-if (randomNum1 > randomNum2) {
-    document.querySelector(".result").innerHTML = "Winner: Player 🅰️";
-    document.querySelector(".img1").style.cssText = "border: 5px solid green; border-radius: 50px";
+  if (randomNum1 > randomNum2) {
+    document.querySelector(".result").innerHTML = "Winner: Player 🅰";
+    document.querySelector(".img1").style.cssText = "border: 5px solid green; border-radius: 20px";
+    document.querySelector(".img2").style.cssText = "none";
+  } else if (randomNum1 < randomNum2) {
+    document.querySelector(".result").innerHTML = "Winner: Player 🅱";
+    document.querySelector(".img2").style.cssText = "border: 5px solid green; border-radius: 20px";
+    document.querySelector(".img1").style.cssText = "none";
 
-} else if (randomNum1 < randomNum2) {
-    document.querySelector(".result").innerHTML = "Winner: Player 🅱️";
-    document.querySelector(".img2").style.cssText = "border: 5px solid green; border-radius: 50px";
-
-} else {
-    document.querySelector(".result").innerHTML = "It's a Tie! 🤝";
+  } else {
+    document.querySelector(".result").innerHTML = "It's a Tie!";
     document.querySelectorAll(".img1, .img2").forEach(function(img){
-    img.style.cssText = "border: 5px solid green; border-radius: 50px";
-    })
-}
+      img.style.cssText = "border: 5px solid pink; border-radius: 20px";
+    });
+  }
+};
+
+document.querySelector(".btn").addEventListener("click", rollDice);
