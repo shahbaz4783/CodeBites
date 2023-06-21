a815b4ddf03f4feef3ee2fd2574984536112f286
shahbaz4783 <shahbaz4783@gmail.com>
2023-06-21 11:51:07 +0530

    clear all added

--- quickList/script.js
+++ quickList/script.js
@@ -1,3 +1,4 @@
+// get elements
 const getInputField = document.getElementById('input-item');
 const getAddBtn = document.getElementById('add-item');
 const getSearchBar = document.getElementById('search-bar');
@@ -5,38 +6,66 @@ const getSearchTab = document.querySelector('.search-group');
 const getClearAllBtn = document.getElementById('clear-all');
 const getSection = document.querySelector('section');
 
+// Initial non required buttons Hide
 getClearAllBtn.style.display = 'none';
 getSearchTab.style.display = 'none';
 getSection.style.display = 'none';
 
+
+// Add and Remove Items
 const addItem = () => {
-    if (getInputField.value === ''){
-        alert('Empty Field')
-    } else {
-        const listBox = document.createElement('div');
-        listBox.className = 'box'
-        getSection.prepend(listBox)
-        
-        const itemName = document.createElement('div');
-        itemName.innerText = getInputField.value;
-        itemName.className = 'item'
-        listBox.append(itemName)
-        getInputField.value = '';
+	if (getInputField.value === '') {
+		alert('Empty Field');
+	} else {
+		const listBox = document.createElement('div');
+		listBox.className = 'box';
+		getSection.prepend(listBox);
 
-        const remove = document.createElement('div');
-        remove.innerHTML = '<svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>';
-        remove.className = 'removeBtn'
-        listBox.append(remove);
-        
-        getSection.style.display = 'flex';
-        getClearAllBtn.style.display = 'block';
-        getSearchTab.style.display = 'block';
+		const itemName = document.createElement('div');
+		itemName.innerText = getInputField.value;
+		itemName.className = 'item';
+		listBox.append(itemName);
+		getInputField.value = '';
 
+		const remove = document.createElement('div');
+		remove.innerHTML =
+			'<svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>';
+		remove.className = 'removeBtn';
+		listBox.append(remove);
 
-        remove.addEventListener('click', () => {
-            listBox.remove();
-        })
-    }
+		getSection.style.display = 'flex';
+		getClearAllBtn.style.display = 'block';
+		getSearchTab.style.display = 'block';
+
+		remove.addEventListener('click', () => {
+			listBox.remove();
+			if (getSection.children.length === 0) {
+				getSection.style.display = 'none';
+				getSearchTab.style.display = 'none';
+				getClearAllBtn.style.display = 'none';
+			}
+		});
+	}
 };
 
+// Reset Website UI
+const resetUI = () => {
+	while (getSection.firstChild) {
+		getSection.firstChild.remove();
+	}
+	getSearchTab.style.display = 'none';
+	getSection.style.display = 'none';
+	getClearAllBtn.style.display = 'none';
+};
+
+
+// Search Items
+const findItems = () => {
+
+}
+
+
+
 getAddBtn.addEventListener('click', addItem);
+getClearAllBtn.addEventListener('click', resetUI);
+getSearchBar.addEventListener('click', findItems)
