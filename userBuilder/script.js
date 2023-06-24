40ec1847eb748f123c8cf1cb6cadba99cbaa032b
shahbaz4783 <shahbaz4783@gmail.com>
2023-06-24 20:06:03 +0530

    mobile responsive

--- userBuilder/script.js
+++ userBuilder/script.js
@@ -8,39 +8,52 @@ const getCountry = document.querySelector('.country');
 const getPhone = document.querySelector('.phone');
 
 const fetchUser = () => {
-    fetch('https://randomuser.me/api/')
-    
-    .then((response) => {
-        return response.json();
-    })
-    .then((data)=> {
-        displayUser(data.results[0])
-    })
-}
+	showSpinner();
+	hideCard();
+	fetch('https://randomuser.me/api/')
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			hideSpinner();
+			showCard();
+			displayUser(data.results[0]);
+		});
+};
 
 const displayUser = (user) => {
-    const fullName = `${user.name.first} ${user.name.last}`;
-    const age = user.dob.age;
-    const gender = user.gender;
-    const image = user.picture.large;
-    const country = user.location.country;
-    const emailID = user.email;
-    const phone = user.phone;
+	const fullName = `${user.name.first} ${user.name.last}`;
+	const age = user.dob.age;
+	const gender = user.gender;
+	const image = user.picture.large;
+	const country = user.location.country;
+	const emailID = user.email;
+	const phone = user.phone;
 
-    getName.innerHTML = fullName;
-    getAge.innerHTML = age;
-    getGender.innerHTML = gender;
-    getImageBox.src = image;
-    getCountry.innerHTML = country;
-    getEmail.innerHTML = emailID;
-    getPhone.innerHTML = phone;
+	getName.innerHTML = fullName;
+	getAge.innerHTML = age;
+	getGender.innerHTML = gender;
+	getImageBox.src = image;
+	getCountry.innerHTML = country;
+	getEmail.innerHTML = emailID;
+	getPhone.innerHTML = phone;
+};
 
-    if (user.gender === 'male'){
-        document.body.style.backgroundColor = '#68D8D6';
-    } else {
-        document.body.style.backgroundColor = '#0077b6';
-    }
-}
+// Spinner
+const showSpinner = () => {
+	document.querySelector('.loading').style.display = 'flex';
+};
+const hideSpinner = () => {
+	document.querySelector('.loading').style.display = 'none';
+};
 
-getBtn.addEventListener('click', fetchUser)
+// card
+const showCard = () => {
+	document.querySelector('.card').style.display = 'block';
+};
+const hideCard = () => {
+	document.querySelector('.card').style.display = 'none';
+};
+
+getBtn.addEventListener('click', fetchUser);
 document.addEventListener('DOMContentLoaded', fetchUser);
