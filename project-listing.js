9a61f941fbf5e8005d6c3a34ddf4ba702d085721
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-04 11:05:06 +0530

    card bug fixes

--- project-listing.js
+++ project-listing.js
@@ -19,8 +19,7 @@ new Swiper('.mySwiper', {
 
 
   new Swiper(".mySwiper3", {
-    slidesPerView: 5,
-    spaceBetween: 30,
+    slidesPerView: 4,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -29,5 +28,23 @@ new Swiper('.mySwiper', {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
+      breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 2,
+        },
+        200: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+        },
+        300: {
+            slidesPerView: 3,
+            spaceBetween: 8,
+        },
+        600: {
+            slidesPerView: 4,
+            spaceBetween: 60,
+        }
+    },
      
   });
