e0507a7d1a7d477cb04094d7185fddfbee10f292
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-05 23:31:27 +0530

    bugs fixes

--- colorEcho/script.js
+++ colorEcho/script.js
@@ -5,26 +5,28 @@ const rec = new SpeechRecognition();
 rec.lang = 'en-US';
 rec.continuous = true;
 
-rec.onresult = (e) => {
-    const colors = [
-        'red',
-        'blue',
-        'green',
-        'pink',
-        'black',
-        'orange',
-        'purple',
-        'white',
-        'gray'
-    ];
+rec.onstart = () => {
+    document.querySelector('h2').innerHTML = 'Hearing...';
+};
 
+rec.onend = () => {
+    document.querySelector('h2').innerHTML = 'Reload to Start Again';
+    document.querySelector('p').innerHTML = '';
+
+};
+
+rec.onresult = (e) => {
     const latestResult = e.results[e.results.length - 1][0].transcript.toLowerCase().trim();
     document.querySelector('p').innerHTML = latestResult;
-
-    if (colors.includes(latestResult)) {
+    
+    if (latestResult === 'black'){
         document.body.style.backgroundColor = latestResult;
+        document.body.style.color = 'white';
+        document.querySelector('h2').style.borderColor = 'white'
     } else {
-        alert('Please say a color name.');
+        document.body.style.backgroundColor = latestResult; 
+        document.body.style.color = 'black';
+        document.querySelector('h2').style.borderColor = 'black'
     }
 };
 
