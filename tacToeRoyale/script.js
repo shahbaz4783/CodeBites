0bbac99a63ec97c30fcabdf3d1461246cd9ad10e
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-15 19:18:19 +0530

    result pop up

--- tacToeRoyale/script.js
+++ tacToeRoyale/script.js
@@ -4,70 +4,90 @@ const stats = document.querySelector('.stats');
 const intro = document.querySelector('.intro');
 const result = document.querySelector('.result');
 const playBtn = document.querySelector('.playBtn');
+const playAgainBtn = document.querySelector('.playAgain');
+const exit = document.querySelector('.exit');
 
 // hide initial sections
-gameBoard.style.display = 'none'
-stats.style.display = 'none'
-result.style.display = 'none'
+gameBoard.style.display = 'none';
+stats.style.display = 'none';
+result.style.display = 'none';
 
 // playnow
 playBtn.addEventListener('click', () => {
-    gameBoard.style.display = 'grid';
-    stats.style.display = 'flex';
-    intro.style.display = 'none';
-})
+	gameBoard.style.display = 'grid';
+	stats.style.display = 'flex';
+	intro.style.display = 'none';
+});
 
 let currentPlayer = 'X';
 
-boardBox.forEach(box => {
-    box.addEventListener('click', () => {
-        if (box.innerHTML === '') {
-            box.innerHTML = currentPlayer;
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            checkGameStatus();
-            setTimeout(() => {
-                computerMove();
-            }, 1000);
-        }
-    });
+boardBox.forEach((box) => {
+	box.addEventListener('click', () => {
+		if (box.innerHTML === '') {
+			box.innerHTML = currentPlayer;
+			currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+			checkGameStatus();
+			setTimeout(() => {
+				computerMove();
+			}, 500);
+		}
+	});
 });
 
 const checkGameStatus = () => {
-    const winningCombos = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ];
+	const winningCombos = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
 
-    // Check for a win
-    for (let combo of winningCombos) {
-        const [a, b, c] = combo;
-        if (
-            boardBox[a].innerHTML !== '' &&
-            boardBox[a].innerHTML === boardBox[b].innerHTML &&
-            boardBox[a].innerHTML === boardBox[c].innerHTML
-        ) {
-            result.style.display = 'block';
-            result.querySelector('p').textContent = `${boardBox[a].innerHTML} wins!`;
-            return;
+
+    setTimeout(() => {
+        // Check for a win
+        for (let combo of winningCombos) {
+            const [a, b, c] = combo;
+            if (boardBox[a].innerHTML !== '' && boardBox[a].innerHTML === boardBox[b].innerHTML && boardBox[a].innerHTML === boardBox[c].innerHTML) {
+                gameBoard.classList.add('blurBackground')
+                result.style.display = 'block';
+                result.querySelector('p').textContent = `${boardBox[a].innerHTML == 'X' ? 'You Won!' : 'You Lost'}`;
+                return;
+            }
         }
-    }
-
-    // Check for a tie
-    if (Array.from(boardBox).every(box => box.innerHTML !== '')) {
-        result.style.display = 'block';
-        result.querySelector('p').textContent = "It's a tie!";
-    }
-}
+        // Check for a tie
+        if (Array.from(boardBox).every((box) => box.innerHTML !== '')) {
+            gameBoard.classList.add('blurBackground')
+            result.style.display = 'block';
+            result.querySelector('p').textContent = "It's a tie!";
+        }
+    }, 3000);
+};
 
 const computerMove = () => {
-    const randomIndex = Math.floor(Math.random() * 9);
+	const randomIndex = Math.floor(Math.random() * 9);
 
-    if (boardBox[randomIndex].innerHTML === '') {
-        boardBox[randomIndex].innerHTML = 'O';
-        currentPlayer = 'X';
-        checkGameStatus();
-    } else {
-        computerMove();
-    }
-}
+	if (boardBox[randomIndex].innerHTML === '') {
+		boardBox[randomIndex].innerHTML = 'O';
+		currentPlayer = 'X';
+		checkGameStatus();
+	} else {
+		computerMove();
+	}
+};
+
+
+playAgainBtn.addEventListener('click', () => {
+    boardBox.forEach((box) => {
+        box.innerHTML = '';
+    })
+    result.style.display = 'none';
+    gameBoard.classList.remove('blurBackground')
+})
+
+exit.addEventListener('click', () => {
+    window.location.href = 'https://prolio.netlify.app/';
+})
